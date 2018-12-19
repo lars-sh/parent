cf5463b460ecb8b724cac46408017313b0e3ee89
Lars Knickrehm <mail@lars-sh.de>
2018-12-20 00:35:23 +0100

    Migrated GlobalProxyConfiguration from enum to class

--- utils/src/main/lombok/de/larssh/utils/net/GlobalProxyConfiguration.java
+++ utils/src/main/lombok/de/larssh/utils/net/GlobalProxyConfiguration.java
@@ -12,12 +12,12 @@ import java.util.Optional;
 import java.util.Set;
 
 import de.larssh.utils.text.Strings;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
- * Enumeration of network protocols that allow proxy related properties,
- * described at <a href=
+ * Proxy related properties, described at <a href=
  * "https://docs.oracle.com/javase/8/docs/api/java/net/doc-files/net-properties.html">Network
  * Properties</a>.
  *
@@ -38,27 +38,34 @@ import lombok.RequiredArgsConstructor;
  * </pre>
  */
 @Getter
-@RequiredArgsConstructor
-public enum GlobalProxyConfiguration {
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class GlobalProxyConfiguration {
+	/**
+	 * Default {@code nonProxyHosts} for {@link #FTP} and {@link #HTTP}
+	 */
+	private static final Collection<String> NO_PROXY_HOSTS_DEFAULT = Arrays.asList("localhost", "127.*", "[::1]");
+
 	/**
 	 * FTP proxy related settings
 	 */
-	FTP(Type.HTTP,
+	public static final GlobalProxyConfiguration FTP = new GlobalProxyConfiguration("FTP",
+			Type.HTTP,
 			80,
 			"ftp.proxyHost",
 			"ftp.proxyPort",
 			Optional.of("ftp.nonProxyHosts"),
-			Optional.of(Arrays.asList("localhost", "127.*", "[::1]"))),
+			Optional.of(NO_PROXY_HOSTS_DEFAULT));
 
 	/**
 	 * HTTP proxy related settings
 	 */
-	HTTP(Type.HTTP,
+	public static final GlobalProxyConfiguration HTTP = new GlobalProxyConfiguration("HTTP",
+			Type.HTTP,
 			80,
 			"http.proxyHost",
 			"http.proxyPort",
 			Optional.of("http.nonProxyHosts"),
-			Optional.of(Arrays.asList("localhost", "127.*", "[::1]"))),
+			Optional.of(NO_PROXY_HOSTS_DEFAULT));
 
 	/**
 	 * HTTPS proxy related settings
@@ -67,44 +74,75 @@ public enum GlobalProxyConfiguration {
 	 * The HTTPS protocol handler will use the same {@code nonProxyHosts} property
 	 * as the HTTP protocol. Use {@link #HTTP} to modify that property.
 	 */
-	HTTPS(Type.HTTP, 443, "https.proxyHost", "https.proxyPort", Optional.empty(), Optional.empty()),
+	public static final GlobalProxyConfiguration HTTPS = new GlobalProxyConfiguration("HTTPS",
+			Type.HTTP,
+			443,
+			"https.proxyHost",
+			"https.proxyPort",
+			Optional.empty(),
+			Optional.empty());
 
 	/**
 	 * SOCKS proxy related properties
 	 *
 	 * <p>
-	 * The SOCKS protocol handler does not support the {@ocde nonProxyHosts}
+	 * The SOCKS protocol handler does not support the {@code nonProxyHosts}
 	 * property.
 	 */
-	SOCKS(Type.SOCKS, 1080, "socksProxyHost", "socksProxyPort", Optional.empty(), Optional.empty());
+	public static final GlobalProxyConfiguration SOCKS = new GlobalProxyConfiguration("SOCKS",
+			Type.SOCKS,
+			1080,
+			"socksProxyHost",
+			"socksProxyPort",
+			Optional.empty(),
+			Optional.empty());
+
+	/**
+	 * Name
+	 *
+	 * @return name
+	 */
+	String name;
 
 	/**
 	 * Proxy type
+	 *
+	 * @return proxy type
 	 */
 	Type type;
 
 	/**
 	 * Default proxy port
+	 *
+	 * @return default proxy port
 	 */
 	int defaultPort;
 
 	/**
 	 * Host property name
+	 *
+	 * @return host property name
 	 */
 	String hostProperty;
 
 	/**
 	 * Port property name
+	 *
+	 * @return port property name
 	 */
 	String portProperty;
 
 	/**
 	 * {@code nonProxyHosts} property name
+	 *
+	 * @return {@code nonProxyHosts} property name
 	 */
 	Optional<String> nonProxyHostsProperty;
 
 	/**
 	 * Default {@code nonProxyHosts}
+	 *
+	 * @return default {@code nonProxyHosts}
 	 */
 	Optional<Collection<String>> defaultNonProxyHosts;
 
@@ -154,9 +192,9 @@ public enum GlobalProxyConfiguration {
 	 * @throws UnsupportedOperationException for protocols not supporting the
 	 *                                       {@code nonProxyHosts} property
 	 */
-	private String getNonProxyHostsPropertyOrThrow() {
+	protected String getNonProxyHostsPropertyOrThrow() {
 		return getNonProxyHostsProperty().orElseThrow(() -> new UnsupportedOperationException(
-				Strings.format("The protocol %s does not support the nonProxyHosts property.", name())));
+				Strings.format("The protocol %s does not support the nonProxyHosts property.", getName())));
 	}
 
 	/**
