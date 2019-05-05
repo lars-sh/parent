d2cd74099c4c7521cc73991f2cdd50fdbd16b66a
Lars Knickrehm <mail@lars-sh.de>
2019-05-05 14:09:07 +0200

    Add rules.xml to toots to avoid updating to pre-release versions

--- tools/update-dependencies.cmd
+++ tools/update-dependencies.cmd
@@ -7,10 +7,10 @@ rem Change directory to project root
 cd /d "%~dp0\.."
 
 rem Update version properties
-call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DgenerateBackupPoms=false
+call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DgenerateBackupPoms=false -Dmaven.version.rules="file:///%old_cd%/rules.xml"
 
 rem Update version tags
-call mvn org.codehaus.mojo:versions-maven-plugin:use-latest-releases -DgenerateBackupPoms=false
+call mvn org.codehaus.mojo:versions-maven-plugin:use-latest-releases -DgenerateBackupPoms=false -Dmaven.version.rules="file:///%old_cd%/rules.xml"
 
 rem Revert working directory and pause
 cd /d "%old_cd%"
