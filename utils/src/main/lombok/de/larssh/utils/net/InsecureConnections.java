eb5ae0a32ddc91535c4f7386bc80a012eefa351f
Lars Knickrehm <mail@lars-sh.de>
2019-07-20 10:52:45 +0200

    Fix SpotBugs and PMD warnings

--- utils/src/main/lombok/de/larssh/utils/net/InsecureConnections.java
+++ utils/src/main/lombok/de/larssh/utils/net/InsecureConnections.java
@@ -7,6 +7,8 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
 import edu.umd.cs.findbugs.annotations.Nullable;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.experimental.UtilityClass;
 
@@ -16,12 +18,6 @@ import lombok.experimental.UtilityClass;
  */
 @UtilityClass
 public class InsecureConnections {
-	/**
-	 * Array of {@link TrustManager}s that <b>do not verify certificates</b>
-	 */
-	private static final TrustManager[] TRUST_MANAGERS_THAT_DO_NOT_VERIFY_CERTIFICATES
-			= new TrustManager[] { new TrustManagerThatDoesNotVerifyCertificates() };
-
 	/**
 	 * {@link HostnameVerifier} that <b>does not verify host names</b>
 	 */
@@ -34,8 +30,9 @@ public class InsecureConnections {
 	 *
 	 * @return array of {@link TrustManager}s that <b>do not verify certificates</b>
 	 */
+	@SuppressWarnings("PMD.TRUST_MANAGERS_THAT_DO_NOT_VERIFY_CERTIFICATES")
 	public static TrustManager[] getTrustManagersThatDoNotVerifyCertificates() {
-		return TRUST_MANAGERS_THAT_DO_NOT_VERIFY_CERTIFICATES;
+		return new TrustManager[] { TrustManagerThatDoesNotVerifyCertificates.getInstance() };
 	}
 
 	/**
@@ -51,15 +48,31 @@ public class InsecureConnections {
 	/**
 	 * {@link TrustManager} that <b>does not verify certificates</b>
 	 */
-	@NoArgsConstructor
+	@NoArgsConstructor(access = AccessLevel.PRIVATE)
 	private static final class TrustManagerThatDoesNotVerifyCertificates implements X509TrustManager {
 		/**
 		 * Empty array of acceptable CA issuer certificates
 		 */
 		private static final X509Certificate[] ACCEPTED_ISSUERS = new X509Certificate[0];
 
+		/**
+		 * Singleton instance
+		 */
+		private static final TrustManager INSTANCE = new TrustManagerThatDoesNotVerifyCertificates();
+
+		/**
+		 * {@link TrustManager} that <b>does not verify certificates</b>
+		 *
+		 * @return {@link TrustManager} that <b>does not verify certificates</b>
+		 */
+		public static TrustManager getInstance() {
+			return INSTANCE;
+		}
+
 		/** {@inheritDoc} */
 		@Override
+		@SuppressFBWarnings(value = "WEAK_TRUST_MANAGER",
+				justification = "The check is absolutely right. That's why this is part of the utility class InsecureConnections.")
 		public void checkClientTrusted(@SuppressWarnings("unused") @Nullable final X509Certificate[] chain,
 				@SuppressWarnings("unused") @Nullable final String authenticationType) {
 			// do not verify client certificates
@@ -67,6 +80,8 @@ public class InsecureConnections {
 
 		/** {@inheritDoc} */
 		@Override
+		@SuppressFBWarnings(value = "WEAK_TRUST_MANAGER",
+				justification = "The check is absolutely right. That's why this is part of the utility class InsecureConnections.")
 		public void checkServerTrusted(@SuppressWarnings("unused") @Nullable final X509Certificate[] chain,
 				@SuppressWarnings("unused") @Nullable final String authenticationType) {
 			// do not verify server certificates
@@ -74,6 +89,8 @@ public class InsecureConnections {
 
 		/** {@inheritDoc} */
 		@Override
+		@SuppressFBWarnings(value = "WEAK_TRUST_MANAGER",
+				justification = "The check is absolutely right. That's why this is part of the utility class InsecureConnections.")
 		public X509Certificate[] getAcceptedIssuers() {
 			return ACCEPTED_ISSUERS;
 		}
