e0b9103d02416dcf243972da91011c0c079750d4
Lars Knickrehm <mail@lars-sh.de>
2021-02-17 17:22:56 +0100

    Suppress github checksum warning

---
+++ mvn-suppressions.sh
@@ -0,0 +1,7 @@
+function suppressGithubChecksumWarnings() {
+	cat < /dev/stdin \
+	| grep --invert-match --perl-regexp "^Warning:  Checksum validation failed, expected [a-z0-9]+ but is [a-z0-9]+ from github for https://maven.pkg.github.com/lars-sh/parent/de/lars-sh/parent/maven-metadata.xml$"
+}
+
+cat < /dev/stdin \
+| suppressGithubChecksumWarnings
