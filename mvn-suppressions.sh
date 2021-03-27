65df3eb35615177f2fbc1b158ae23f20e797aa6e
Lars Knickrehm <mail@lars-sh.de>
2021-03-27 02:02:32 +0100

    Fix building the master branch

--- mvn-suppressions.sh
+++ mvn-suppressions.sh
@@ -1,6 +1,6 @@
 function suppressGithubChecksumWarnings() {
 	cat < /dev/stdin \
-	| grep --invert-match --perl-regexp "^Warning:  Checksum validation failed, expected [a-z0-9]+ but is [a-z0-9]+ from github for https://maven.pkg.github.com/lars-sh/parent/de/lars-sh/parent/maven-metadata.xml$"
+	| grep --invert-match --perl-regexp "^Warning:  Checksum validation failed, expected [a-z0-9]+ but is [a-z0-9]+ from github for https://maven.pkg.github.com/lars-sh/parent/de/lars-sh/[^/]+/maven-metadata.xml$"
 }
 
 cat < /dev/stdin \
